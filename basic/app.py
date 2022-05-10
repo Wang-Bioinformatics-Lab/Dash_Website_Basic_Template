67324b433d62272b036e74528dd2069634684393
Mingxun Wang <mwang87@gmail.com>
2022-05-09 18:24:43 -0700

    refactor

--- basic/app.py
+++ basic/app.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import dash
-import dash_core_components as dcc
+from dash import dcc
+from dash import html
+from dash import dash_table
 import dash_bootstrap_components as dbc
-import dash_html_components as html
-import dash_table
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import plotly.graph_objects as go 
-from dash.dependencies import Input, Output, State
+
 import os
-from zipfile import ZipFile
 import urllib.parse
 from flask import Flask, send_from_directory
 
@@ -30,7 +30,7 @@ from flask_caching import Cache
 
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = 'GNPS - Template'
+app.title = 'Wang Lab - Dashboard Template'
 
 cache = Cache(app.server, config={
     'CACHE_TYPE': 'filesystem',
@@ -45,11 +45,11 @@ NAVBAR = dbc.Navbar(
     children=[
         dbc.NavbarBrand(
             html.Img(src="https://gnps-cytoscape.ucsd.edu/static/img/GNPS_logo.png", width="120px"),
-            href="https://gnps.ucsd.edu"
+            href="https://mingxunwang.com"
         ),
         dbc.Nav(
             [
-                dbc.NavItem(dbc.NavLink("GNPS - Template Dashboard - Version 0.1", href="#")),
+                dbc.NavItem(dbc.NavLink("Wang Bioinformatics Lab - Template Dashboard - Version 0.1", href="#")),
             ],
         navbar=True)
     ],
@@ -62,10 +62,10 @@ DATASELECTION_CARD = [
     dbc.CardHeader(html.H5("Data Selection")),
     dbc.CardBody(
         [   
-            html.H5(children='GNPS Data Selection'),
+            html.H5(children='Data Selection'),
             dbc.InputGroup(
                 [
-                    dbc.InputGroupAddon("Spectrum USI", addon_type="prepend"),
+                    dbc.InputGroupText("Spectrum USI"),
                     dbc.Input(id='usi1', placeholder="Enter GNPS USI", value=""),
                 ],
                 className="mb-3",
@@ -73,7 +73,7 @@ DATASELECTION_CARD = [
             html.Hr(),
             dbc.InputGroup(
                 [
-                    dbc.InputGroupAddon("Spectrum USI", addon_type="prepend"),
+                    dbc.InputGroupText("Spectrum USI"),
                     dbc.Input(id='usi2', placeholder="Enter GNPS USI", value=""),
                 ],
                 className="mb-3",
@@ -107,7 +107,7 @@ CONTRIBUTORS_DASHBOARD = [
     dbc.CardHeader(html.H5("Contributors")),
     dbc.CardBody(
         [
-            "Mingxun Wang PhD - UC San Diego",
+            "Mingxun Wang PhD - UC Riverside",
             html.Br(),
             html.Br(),
             html.H5("Citation"),
@@ -172,75 +172,6 @@ def determine_task(search):
     usi2 = _get_url_param(query_dict, "usi2", 'mzspec:MSV000082796:KP_108_Positive:scan:1977')
 
     return [usi1, usi2]
-
-
-# @app.callback([
-#                 Output('plot_link', 'children')
-#               ],
-#               [
-#                   Input('gnps_tall_table_usi', 'value'),
-#                   Input('gnps_quant_table_usi', 'value'),
-#                   Input('gnps_metadata_table_usi', 'value'), 
-#                 Input('feature', 'value'),
-#                 Input("metadata_column", "value"),
-#                 Input("facet_column", "value"),
-#                 Input("animation_column", "value"),
-#                 Input("group_selection", "value"),
-#                 Input("color_selection", "value"),
-#                 Input("theme", "value"),
-#                 Input("plot_type", "value"),
-#                 Input("image_export_format", "value"),
-#                 Input("points_toggle", "value"),
-#                 Input("log_axis", "value"),
-#                 Input("lat_column", "value"),
-#                 Input("long_column", "value"),
-#                 Input("map_animation_column", "value"),
-#                 Input("map_scope", "value"),
-#               ])
-# def draw_link(      gnps_tall_table_usi, 
-#                     gnps_quant_table_usi, 
-#                     gnps_metadata_table_usi, 
-#                     feature_id, 
-#                     metadata_column, 
-#                     facet_column, 
-#                     animation_column, 
-#                     group_selection, 
-#                     color_selection, 
-#                     theme, 
-#                     plot_type, 
-#                     image_export_format, 
-#                     points_toggle, 
-#                     log_axis,
-#                     lat_column,
-#                     long_column,
-#                     map_animation_column,
-#                     map_scope):
-#     # Creating Reproducible URL for Plot
-#     url_params = {}
-#     url_params["gnps_tall_table_usi"] = gnps_tall_table_usi
-#     url_params["gnps_quant_table_usi"] = gnps_quant_table_usi
-#     url_params["gnps_metadata_table_usi"] = gnps_metadata_table_usi
-
-#     url_params["feature"] = feature_id
-#     url_params["metadata"] = metadata_column
-#     url_params["facet"] = facet_column
-#     url_params["groups"] = ";".join(group_selection)
-#     url_params["plot_type"] = plot_type
-#     url_params["color"] = color_selection
-#     url_params["points_toggle"] = points_toggle
-#     url_params["theme"] = theme
-#     url_params["animation_column"] = animation_column
-
-#     # Mapping Options
-#     url_params["lat_column"] = lat_column
-#     url_params["long_column"] = long_column
-#     url_params["map_animation_column"] = map_animation_column
-#     url_params["map_scope"] = map_scope
-    
-#     url_provenance = dbc.Button("Link to this Plot", block=True, color="primary", className="mr-1")
-#     provenance_link_object = dcc.Link(url_provenance, href="/?" + urllib.parse.urlencode(url_params) , target="_blank")
-
-#     return [provenance_link_object]
 
 
 @app.callback([
