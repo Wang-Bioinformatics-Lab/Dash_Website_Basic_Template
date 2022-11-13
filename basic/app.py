daef6d462172fad8aa9996f6c1e9f43bb08b98a4
Mingxun Wang <mwang87@gmail.com>
2022-11-13 11:37:14 -0800

    bug fix

--- basic/app.py
+++ basic/app.py
@@ -122,7 +122,7 @@ CONTRIBUTORS_DASHBOARD = [
             html.Br(),
             html.H5("Citation"),
             html.A('Mingxun Wang, Jeremy J. Carver, Vanessa V. Phelan, Laura M. Sanchez, Neha Garg, Yao Peng, Don Duy Nguyen et al. "Sharing and community curation of mass spectrometry data with Global Natural Products Social Molecular Networking." Nature biotechnology 34, no. 8 (2016): 828. PMID: 27504778', 
-                    href="https://www.nature.com/articles/nbt.3597")
+                    href="https://www.nature.com/articles/nbt.3597"),
             html.Br(),
             html.Br(),
             html.A('Checkout our other work!', 
